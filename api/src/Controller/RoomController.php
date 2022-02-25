c0ba6e71cb71cd7b5013b6b15672d7cb43eab533
Dzejkobik007 <kuba.rybnicek@gmail.com>
2022-02-25 19:17:28 +0100

    Add RoomController

---
+++ api/src/Controller/RoomController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Src\Controller;
+
+use Src\TableGateways\RoomGateway;
+use Src\TableGateways\MessageGateway;
+
+class RoomController
+{
+
+    private $db;
+    private $requestMethod;
+    private $userId;
+
+    private $roomGateway;
+
+    public function __construct($db, $requestMethod, $roomId, $message = false, Array $auth = null)
+    {
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+        $this->roomId = $roomId;
+        $this->auth = $auth;
+        $this->message = $message;
+
+        $this->roomGateway = new RoomGateway($db);
+        $this->messageGateway = new MessageGateway($db);
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                if ($this->message) {
+                    if ($this->roomId) {
+                        $response = $this->getMessages($this->roomId);
+                    } else {
+                        $this->unprocessableEntityResponse();
+                    }
+                } else {
+                    if ($this->roomId) {
+                        $response = $this->getRoom($this->roomId);
+                    } else {
+                        $response = $this->getRooms();
+                    }
+                }
+                break;
+            case 'PUT':
+                if ($this->message) {
+                    $response = $this->createMessage($this->roomId);
+                } else {
+                    $response = $this->createRoom();
+                }
+                break;
+            case 'DELETE':
+                $response = $this->deleteRoom($this->roomId);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getMessages($roomId)
+    {
+        $result = $this->messageGateway->findAll($roomId);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getMessage($id)
+    {
+        $result = $this->roomGateway->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function createMessage()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validateMessage($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->messageGateway->insert($input, $this->roomId);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function getRooms()
+    {
+        $result = $this->roomGateway->findAll($this->auth["user"]);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getRoom($id)
+    {
+        $result = $this->roomGateway->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function createRoom()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validateRoom($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->roomGateway->insert($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function updateRoom($id)
+    {
+        $result = $this->roomGateway->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validateRoom($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->roomGateway->update($id, $input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function deleteRoom($id)
+    {
+        $result = $this->roomGateway->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $this->roomGateway->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    // (*name, *owner, *visible, password)
+    private function validateRoom($input)
+    {
+        if (!isset($input['name'])) {
+            return false;
+        }
+        if (!isset($input['owner'])) {
+            return false;
+        }
+        if (!isset($input['visible'])) {
+            return false;
+        }
+        return true;
+    }
+
+    // (*text, *sender, reply, *room, file)
+    private function validateMessage($input)
+    {
+        if (!isset($input['text'])) {
+            return false;
+        }
+        if (!isset($input['sender'])) {
+            return false;
+        }
+        return true;
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+}
